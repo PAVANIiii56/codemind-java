afff0716fbe377629749bf174dd8f93e177e3387
PAVANIiii56 <96327277+PAVANIiii56@users.noreply.github.com>
2022-05-27 07:16:08 +0530

    import java.util.Scanner; class min {     public static void main(String args[])     {             int n,i,m=1;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();        int x[]=new int[n];         for(i=0;i<n;i++)             x[i]=sc.nextInt();
            for(i=0;i<n;i++)
            {
            if(x[i]%2!=0)
            {
            System.out.printf("%d ",x[i]);
            }
            }
                for(i=0;i<n;i++)
                {
            if(x[i]%2==0)
            {
            System.out.printf("%d ",x[i]);
            }
                }
        }
    }

---
+++ array_of_add_and_even_elements.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class min
+{
+    public static void main(String args[])
+    {
+            int n,i,m=1;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+       int x[]=new int[n];
+        for(i=0;i<n;i++)
+            x[i]=sc.nextInt();
+
+        for(i=0;i<n;i++)
+        {
+        if(x[i]%2!=0)
+        {
+        System.out.printf("%d ",x[i]);
+        }
+        }
+            for(i=0;i<n;i++)
+            {
+        if(x[i]%2==0)
+        {
+        System.out.printf("%d ",x[i]);
+        }
+            }
+    }
+}
