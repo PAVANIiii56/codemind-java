76f7cf2e8b559e33270c0f4bffb19e26c1d734c3
PAVANIiii56 <96327277+PAVANIiii56@users.noreply.github.com>
2022-08-26 19:28:35 +0530

    import java.util.Scanner; class Sample {     public static void main(String pavs[])     {         Scanner sc=new Scanner(System.in);      int n,i;      n=sc.nextInt();      int a[]=new int[n];     for(i=0;i<n;i++)     {        a[i]=sc.nextInt();     }
         for(i=n-1;i>=0;i--)
         {
            if(a[i]%2==0)
            {
         System.out.println(a[i]);
         break;
            }
         }
        }
    }

---
+++ Last_even_number_in_an_array.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Sample
+{
+    public static void main(String pavs[])
+    {
+        Scanner sc=new Scanner(System.in);
+     int n,i;  
+     n=sc.nextInt();
+     int a[]=new int[n];
+    for(i=0;i<n;i++)
+    {
+       a[i]=sc.nextInt();
+    }
+    
+     for(i=n-1;i>=0;i--)
+     {
+        if(a[i]%2==0)
+        {
+     System.out.println(a[i]);
+     break;
+        }
+     }
+    }
+}
