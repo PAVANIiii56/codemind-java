643d7c6d3ad97dbade53544d74072c3960caf3e9
PAVANIiii56 <96327277+PAVANIiii56@users.noreply.github.com>
2022-08-26 20:33:47 +0530

    import java.util.Scanner; class Sample {     public static void main(String pavs[])     {         Scanner sc=new Scanner(System.in);      int n,i,sum_e=0,sum_o=0;      n=sc.nextInt();      int a[]=new int[n];     for(i=0;i<n;i++)     {        a[i]=sc.nextInt();     }      for(i=0;i<n;i++)      {          if(i%2==0)          {              sum_e=sum_e+a[i];          }      }
         for(i=0;i<n;i++)
         {
             if(i%2!=0)
             {
                 sum_o=sum_o+a[i];
             }
         }
         
             if(sum_e>sum_o)
             {
                 System.out.format("%d",sum_e-sum_o);
             }
            else
         
             System.out.format("%d",sum_o-sum_e);
         
        } 
    }

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_indexed_elements.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+class Sample
+{
+    public static void main(String pavs[])
+    {
+        Scanner sc=new Scanner(System.in);
+     int n,i,sum_e=0,sum_o=0;
+     n=sc.nextInt();
+     int a[]=new int[n];
+    for(i=0;i<n;i++)
+    {
+       a[i]=sc.nextInt();
+    }
+     for(i=0;i<n;i++)
+     {
+         if(i%2==0)
+         {
+             sum_e=sum_e+a[i];
+         }
+     }
+  
+     for(i=0;i<n;i++)
+     {
+         if(i%2!=0)
+         {
+             sum_o=sum_o+a[i];
+         }
+     }
+     
+         if(sum_e>sum_o)
+         {
+             System.out.format("%d",sum_e-sum_o);
+         }
+        else
+     
+         System.out.format("%d",sum_o-sum_e);
+     
+    } 
+}
