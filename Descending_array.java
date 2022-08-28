a88686f98afcdceb2a24db0d53111700047e8280
PAVANIiii56 <96327277+PAVANIiii56@users.noreply.github.com>
2022-08-28 14:38:38 +0530

    import java.util.Scanner; class Sample {     public static void main(String pavs[])     {         Scanner sc=new Scanner(System.in);         int n,a[],i,s=0;         n=sc.nextInt();         a=new int[n];         for(i=0;i<n;i++)        {         a[i]=sc.nextInt();        }        for(i=0;i<n-1;i++)        {            if(a[i]>a[i+1])            {                s++;            }        }        if(s==n-1)        {            System.out.println("yes");        }        else         {            System.out.println("no");        }     } }

--- Descending_array.java
+++ Descending_array.java
@@ -1,30 +1,30 @@
 import java.util.Scanner;
-class Descending
+class Sample
 {
-    public static void main(String args[])
+    public static void main(String pavs[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,x[],i,s=0;
+        int n,a[],i,s=0;
         n=sc.nextInt();
-        x=new int[n];
+        a=new int[n];
         for(i=0;i<n;i++)
+       {
+        a[i]=sc.nextInt();
+       }
+       for(i=0;i<n-1;i++)
+       {
+           if(a[i]>a[i+1])
+           {
+               s++;
+           }
+       }
+       if(s==n-1)
+       {
+           System.out.println("yes");
+       }
+       else
         {
-            x[i]=sc.nextInt();
-        }
-        for(i=0;i<n-1;i++)
-        {
-            if(x[i]>x[i+1])
-            {
-                s++;
-            }
-        }
-        if(s==n-1)
-        {
-            System.out.println("yes");
-        }
-        else
-        {
-            System.out.println("no");
-        }
+           System.out.println("no");
+       }
     }
 }
