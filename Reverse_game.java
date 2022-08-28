e1acefbccda5103d8bb5317227be3fcd80a3d838
PAVANIiii56 <96327277+PAVANIiii56@users.noreply.github.com>
2022-08-28 15:25:03 +0530

    import java.util.Scanner; class Sample {     public static int reverse(int num)     {         int r,rev=0;         while(num>0)         {             r=num%10;             rev=rev*10+r;             num=num/10;         }         return rev;     }     public static void main(String pavs[])     {         Scanner sc=new Scanner(System.in);         int n,i,a[];         n=sc.nextInt();         a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             a[i]=Sample.reverse(a[i]);         System.out.format("%d ",a[i]);         }     } }

---
+++ Reverse_game.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class Sample
+{
+    public static int reverse(int num)
+    {
+        int r,rev=0;
+        while(num>0)
+        {
+            r=num%10;
+            rev=rev*10+r;
+            num=num/10;
+        }
+        return rev;
+    }
+    public static void main(String pavs[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,a[];
+        n=sc.nextInt();
+        a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            a[i]=Sample.reverse(a[i]);
+        System.out.format("%d ",a[i]);
+        }
+    }
+}
