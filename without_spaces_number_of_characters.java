9996aa44a7f67a7b127853cafaebc9bb0a14accc
PAVANIiii56 <96327277+PAVANIiii56@users.noreply.github.com>
2022-10-23 23:12:13 +0530

    import java.util.Scanner; class Sample {     public static void main(String pavs[])     {         Scanner sc=new Scanner(System.in);         String s1=sc.nextLine();         int c=0;         for(int i=0;i<s1.length();i++)         {             if(s1.charAt(i)!=' ')             {                 c++;             }         }         System.out.println(c);     } }

---
+++ without_spaces_number_of_characters.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Sample
+{
+    public static void main(String pavs[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+        int c=0;
+        for(int i=0;i<s1.length();i++)
+        {
+            if(s1.charAt(i)!=' ')
+            {
+                c++;
+            }
+        }
+        System.out.println(c);
+    }
+}
