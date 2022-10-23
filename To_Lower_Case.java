4d69bcc62fafd681f45b0d64c375b9cb5749e595
PAVANIiii56 <96327277+PAVANIiii56@users.noreply.github.com>
2022-10-23 21:58:29 +0530

    import java.util.Scanner; class Sample {     public static void main(String pavs[])     {         Scanner sc=new Scanner(System.in);         String s1=sc.nextLine();         String s2;         s2=s1.toLowerCase();         System.out.println(s2);         } }

---
+++ To_Lower_Case.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Sample
+{
+    public static void main(String pavs[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+        String s2;
+        s2=s1.toLowerCase();
+        System.out.println(s2);
+        }
+}
