c938d2047a24e033e8cf1466ed5ffc739fd4c06b
PAVANIiii56 <96327277+PAVANIiii56@users.noreply.github.com>
2022-12-03 22:47:18 +0530

    import java.util.Scanner; class pavani { public static void main(String pavs[]) {     Scanner sc=new Scanner(System.in);     {         int n,c2=0,c=0,v,i,j;         float r;         n=sc.nextInt();         r=(float)((n*1.8)+32);         System.out.printf("%.2f",r);
        }
    }
    }

---
+++ Celsius_To_Fahrenheit.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class pavani
+{
+public static void main(String pavs[])
+{
+    Scanner sc=new Scanner(System.in);
+    {
+        int n,c2=0,c=0,v,i,j;
+        float r;
+        n=sc.nextInt();
+        r=(float)((n*1.8)+32);
+        System.out.printf("%.2f",r);
+        
+    }
+}
+}
