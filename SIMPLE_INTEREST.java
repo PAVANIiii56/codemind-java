bf9f6ae4427902ed78398a57ce99801ccf210504
PAVANIiii56 <96327277+PAVANIiii56@users.noreply.github.com>
2022-12-03 22:38:25 +0530

    import java.util.Scanner; class pavani { public static void main(String pavs[]) {     Scanner sc=new Scanner(System.in);     {         int n,r,c2=0,c=0,v,i,j;         n=sc.nextInt();         r=sc.nextInt();         v=sc.nextInt();         System.out.println((n*r*v)/100);
        }
    }
    }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class pavani
+{
+public static void main(String pavs[])
+{
+    Scanner sc=new Scanner(System.in);
+    {
+        int n,r,c2=0,c=0,v,i,j;
+        n=sc.nextInt();
+        r=sc.nextInt();
+        v=sc.nextInt();
+        System.out.println((n*r*v)/100);
+        
+    }
+}
+}
