da792e533354712ca79548050204a7baa5407e9c
PAVANIiii56 <96327277+PAVANIiii56@users.noreply.github.com>
2022-12-03 20:43:48 +0530

    import java.util.Scanner; class pavani {     public int prime(int n)     {         int i,c=0;         if(n==1)         {             c=1;         }         for(i=2;i<(int)Math.sqrt(n)+1;i++)         {             if(n%i==0)             {                 c+=1;             }         }         return c;     }     public static void main(String pavs[])     {         Scanner sc=new Scanner(System.in);         pavani pa=new pavani();         int n,i,b,c=0;         n=sc.nextInt();         int a[]=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         b=sc.nextInt();         for(i=0;i<n;i++)         {             if(pa.prime(a[i])==0)             {                 if(a[i]<=b)                 c+=1;             }         }         System.out.println(c);     } }

---
+++ primes_less_than_k.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+class pavani
+{
+    public int prime(int n)
+    {
+        int i,c=0;
+        if(n==1)
+        {
+            c=1;
+        }
+        for(i=2;i<(int)Math.sqrt(n)+1;i++)
+        {
+            if(n%i==0)
+            {
+                c+=1;
+            }
+        }
+        return c;
+    }
+    public static void main(String pavs[])
+    {
+        Scanner sc=new Scanner(System.in);
+        pavani pa=new pavani();
+        int n,i,b,c=0;
+        n=sc.nextInt();
+        int a[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        b=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            if(pa.prime(a[i])==0)
+            {
+                if(a[i]<=b)
+                c+=1;
+            }
+        }
+        System.out.println(c);
+    }
+}
